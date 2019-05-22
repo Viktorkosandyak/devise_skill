98352114b4bad74d80ec6624d05347782c13b4bc
Viktorkosandyak <kosandyak1996@gmail.com>
2019-05-22 12:46:22 +0300

    add gem devise_invitable and gem letter_Opener but are some bugs with sign_in

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'devise', '~> 4.1'
 gem 'bootstrap-sass', '~> 3.3'
+gem 'devise_invitable', '~> 2.0.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
@@ -50,6 +51,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'letter_opener'
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+
+# user = User.invite!(email: 'kosandyak1996@gmail.com', username: 'John Doe') do |u|
+#   u.skip_invitation = true
+# end
+#
+#
+# kos.oksana7@gmail.com
+# viktor1234@gmail.com
+
+
+# qwerty@gmail.com
